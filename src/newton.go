6c4382b4e3137a1abdbfb72d38c0bb85f947aa9c
The2R0k <2r0k@bk.ru>
2020-05-05 17:27:35 +0300

    complete "tour of go" example with making Sqrt

---
+++ src/newton.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt";
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	y := x
+	for prev := 0.0; prev != y; {
+		prev = y
+		y = (y+x/y)/2
+	}
+	return y
+}
+
+func main() {
+	fmt.Println(Sqrt(2), math.Sqrt(2))
+}
